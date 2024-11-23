f59e28d2e37366bde2b19c80d8f3100417b3a989
silvadias <silvadias.perfil@outlook.com>
2024-11-22 22:59:34 -0300

    Fechado

--- index.tsx
+++ index.tsx
@@ -1,35 +1,28 @@
-import React, { useState } from "react";
-import { Text, StyleSheet } from "react-native";
-
-const TextoAninhado = () => {
-  const [titulo, setTitulo] = useState("Texto do elemento filho");
-
-  const modificaTexto = () => {
-	setTitulo("Esse texto está sendo exibido pois o primeiro elemento de texto foi pressionado/tocado");
-  };
-
-  return (
-	<Text style={styles.baseText}>
-	  <Text style={styles.titulo} onPress={modificaTexto}>
-		{titulo}
-		{"\n"}
-		{"\n"}
-	  </Text>
-	</Text>
-  );
-};
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  baseText: {
-	fontFamily: "Verdana",
-	marginTop:50,
-	marginLeft:10
+  container: {
+	paddingTop: 50,
   },
-  titulo: {
-	marginTop:10,
-	fontSize: 18,
-	fontWeight: "bold"
+  imagem: {
+	width: 50,
+	height: 50,
+	alignSelf: 'center'
   }
 });
 
-export default TextoAninhado;
+const ComponenteSimplesImage = () => {
+  return (
+	<View style={styles.container}>
+	  <Image
+		style={styles.imagem}
+		source={{
+		  uri: 'https://reactnative.dev/img/tiny_logo.png',
+		}}
+	  />
+	</View>
+  );
+}
+
+export default ComponenteSimplesImage;
