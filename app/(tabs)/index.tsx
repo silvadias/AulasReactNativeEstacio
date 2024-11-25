09932c6906c511698a14d6f302690a500d8bbd1c
silvadias <silvadias.perfil@outlook.com>
2024-11-25 17:53:03 -0300

    feat: react-native-vector-icons/MaterialIcons e react-native-camera, instalação dos dois pacotes com ngrok

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,3 +2,5 @@ import React, {useState} from "react";
 import {Alert, Modal, View, ImageBackground, TouchableOpacity, StyleSheet} from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import {RNCamera} from "react-native-camera";
+
+
